294944b66c518238518251b7187bb740ca6d1b29
Prema Chand Alugu <premaca@gmail.com>
2017-11-01 18:40:11 +0530

    sanders: Define Explicit 4.9 Kernel toolchain

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,6 +56,10 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm
 TARGET_KERNEL_CONFIG := sanders_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
+#
+# Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # Audio
 AUDIO_FEATURE_ENABLED_SND_MONITOR := true
