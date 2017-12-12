aa0d6795ee3e16a847d8f96d6eb2ba094ed321c3
Prema Chand Alugu <premaca@gmail.com>
2017-12-13 04:39:55 +0530

    Revert "sanders: update KeyHandler for api change"
    * DeviceKeyHandler.java, no API change for us yet
    
    This reverts commit 78b0146dc284563431f7c9f10bb0baea468bdeae.

--- LineageActions/src/org/lineageos/settings/device/KeyHandler.java
+++ LineageActions/src/org/lineageos/settings/device/KeyHandler.java
@@ -439,7 +439,7 @@ public class KeyHandler implements DeviceKeyHandler {
         return node;
     }
 
-    public KeyEvent handleKeyEvent(KeyEvent event) {
+    public boolean handleKeyEvent(KeyEvent event) {
         int scanCode = event.getScanCode();
 
         if (DEBUG) {
@@ -453,7 +453,7 @@ public class KeyHandler implements DeviceKeyHandler {
 
         boolean isFPScanCode = ArrayUtils.contains(sSupportedFPGestures, scanCode);
         if (!isFPScanCode) {
-            return event;
+            return false;
         }
 
         boolean isFPGestureEnabled = FileUtils.readOneLine(FP_HOME_NODE).equals("1");
@@ -463,12 +463,12 @@ public class KeyHandler implements DeviceKeyHandler {
 
         // We only want ACTION_UP event
         if (event.getAction() != KeyEvent.ACTION_UP) {
-            return null;
+            return true;
         }
-
+        
         if (isFPScanCode){
             if (fpGesturePending) {
-                return event;
+                return false;
             } else {
                 resetFPGestureDelay();
                 fpGesturePending = true;
@@ -483,7 +483,7 @@ public class KeyHandler implements DeviceKeyHandler {
         if (isFPScanCode) {
             if ((!isFPGestureEnabled) || (!isScreenOn && !isFPGestureEnabledOnScreenOff)) {
                 resetDoubleTapOnFP();
-                return event;
+                return false;
             }
             if (!isScreenOn && isFPGestureEnabledOnScreenOff) {
                 processFPScreenOffScancode(scanCode);
@@ -491,7 +491,7 @@ public class KeyHandler implements DeviceKeyHandler {
                 processFPScancode(scanCode);
             }
         }
-        return null;
+        return true;
     }
 
     private void processFPScancode(int scanCode) {
