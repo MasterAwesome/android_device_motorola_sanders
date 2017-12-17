e1b8f28b6a6e17fe41e49aedb64475d8af8a3479
Vachounet <vachounet@live.fr>
2017-12-17 09:13:53 +0100

    potter: fix custom init

--- init/init_potter.cpp
+++ init/init_potter.cpp
@@ -30,12 +30,14 @@
 #include <stdlib.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
-
+#include <android-base/logging.h>
 #include <android-base/properties.h>
 #include "property_service.h"
-#include "vendor_init.h"
 
-using android::init::property_set;
+
+
+namespace android {
+namespace init {
 
 void property_override(char const prop[], char const value[])
 {
@@ -93,3 +95,5 @@ void vendor_load_properties()
         property_set("ro.hw.dtv", "true");
     }
 }
+}  // namespace init
+} // namespace android
