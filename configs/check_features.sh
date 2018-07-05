2df051b2b1171e03a0acb97717da3787ba70b386
Arvind Mukund <armu30@gmail.com>
2018-07-05 14:29:44 +0530

    sanders: update media configs from stock
    Signed-off-by: Arvind Mukund <armu30@gmail.com>

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -1,30 +1,14 @@
 #!/sbin/sh
 
-# 1802 - Brazil
-# 1803 - Europe
-# 1804 - India
-# 1805 - Europe
-# 1806 - USA
-
 sku=`getprop ro.boot.hardware.sku`
 
-if [ "$sku" = "XT1806" ]; then
-    # XT1806 doesn't have NFC chip
+if [ "$sku" = "XT1687" ]; then
+    # XT1687 doesn't have NFC chip
     rm /system/vendor/etc/permissions/android.hardware.nfc.xml
     rm /system/vendor/etc/permissions/android.hardware.nfc.hce.xml
     rm /system/vendor/etc/permissions/com.android.nfc_extras.xml
     rm -r /system/app/NfcNci
+else
+    # Only XT1687 variant got a compass
+    rm /system/vendor/etc/permissions/android.hardware.sensor.compass.xml
 fi
-
-if ! [ "$sku" = "XT1802" ]; then
-    # Others variants doesn't have DTV support
-    rm /system/vendor/etc/permissions/com.motorola.hardware.dtv.xml
-    rm /system/vendor/etc/permissions/mot_dtv_permissions.xml
-    rm /system/vendor/lib/libdtvtuner.so
-    rm /system/vendor/lib64/libdtvtuner.so
-    rm /system/vendor/lib/libdtvhal.so
-    rm /system/vendor/lib64/libdtvhal.so
-    rm -r /system/vendor/app/DTVPlayer
-    rm -r /system/vendor/app/DTVService
-fi
-
