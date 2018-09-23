c43493a0c8992a682f0eb0632b45187f550bc91a
MasterAwesome <armu30@gmail.com>
2018-09-23 12:13:14 +0530

    Revert "sanders: update fingerprint configs to match vendor changes"
    Stick with 32bit fingerprint for now
    
    This reverts commit ac86423bc51071f3d5984f088e3fc39cf22517c7.

--- device.mk
+++ device.mk
@@ -180,7 +180,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1
+    android.hardware.biometrics.fingerprint@2.1-service_32 \
+    fingerprint.msm8953
 
 # FM
 PRODUCT_PACKAGES += \
