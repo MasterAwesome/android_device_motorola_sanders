e6b2a1b1c953ae300fe8acafe207f08645f42f01
Vachounet <vachounet@live.fr>
2019-01-04 18:36:20 +0100

    potter: add missing softap repo

---
+++ softap/jni/Android.mk
@@ -0,0 +1,20 @@
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_PRELINK_MODULE := false
+
+LOCAL_SRC_FILES := QWiFiSoftApCfg.c
+
+LOCAL_MODULE := libQWiFiSoftApCfg
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include \
+                    $(JNI_H_INCLUDE)
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+LOCAL_SHARED_LIBRARIES := libsysutils libcutils libnetutils libcrypto
+
+include $(BUILD_SHARED_LIBRARY)
