a41964c441cac53ab3f875627d6dc823c0b178bd
Vachounet <vachounet@live.fr>
2019-01-07 13:37:48 +0100

    potter: cleanup duplicate USB hal

--- device.mk
+++ device.mk
@@ -366,10 +366,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine-potter.conf:system/vendor/etc/thermal-engine.conf
 
-# USB HAL
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.potter
