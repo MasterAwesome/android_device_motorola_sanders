696f429bf6885c806f5bf2825effba2604c2a2c8
Arvind Mukund <armu30@gmail.com>
2019-01-11 19:00:04 +0530

    sanders: build correct HALs
    Change-Id: Ie2d3c079160493e2050ea152860d54ae1dd30939
    Signed-off-by: Arvind Mukund <armu30@gmail.com>

--- device.mk
+++ device.mk
@@ -143,7 +143,7 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@2.0-impl \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    android.hardware.configstore@1.0-service \
+    android.hardware.configstore@1.1-service \
     hwcomposer.msm8953 \
     memtrack.msm8953 \
     libgenlock \
@@ -347,7 +347,7 @@ PRODUCT_PACKAGES += \
 
 #RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.0
+    android.hardware.radio@1.1
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
@@ -394,6 +394,7 @@ PRODUCT_PACKAGES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
+    android.hardware.wifi.supplicant@1.0 \
     hostapd \
     libqsap_sdk \
     libwpa_client \
