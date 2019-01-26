f68e2adb75c266202c1fd4f32f8ab763401dca5d
dianlujitao <dianlujitao@lineageos.org>
2019-01-26 20:28:26 +0800

    telephony: Enable workaround for old MCC/MNC format
     * Fixes manual network selection
    
    Change-Id: I104a20491f4a6230c0d142db748e9545b22671a0
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -239,6 +239,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 
 # RIL
 TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
+TARGET_USES_OLD_MNC_FORMAT := true
 
 # SELinux
 #include device/qcom/sepolicy/sepolicy.mk
