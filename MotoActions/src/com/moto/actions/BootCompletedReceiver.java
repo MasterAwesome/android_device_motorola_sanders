f6b0b841d372676f4c2969733e71c909ba9045f4
MSF-Jarvis <msfjarvis@gmail.com>
2019-02-17 15:01:47 +0530

    dirac: Refactor Dirac setup
    * Convert DiracUtils to be an instantiable class
    * Stop tracking initialization state
    
    Signed-off-by: sai4041412 <sai404142@gmail.com>

--- MotoActions/src/com/moto/actions/BootCompletedReceiver.java
+++ MotoActions/src/com/moto/actions/BootCompletedReceiver.java
@@ -51,7 +51,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         }
 
         context.startService(new Intent(context, ServiceWrapper.class));
-        DiracUtils.initialize(context);
+        new DiracUtils(context);
    }
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
