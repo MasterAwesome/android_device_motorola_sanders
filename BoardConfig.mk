214572595ab6f7193463fe5c93debd850eb0c25d
Arvind Mukund <armu30@gmail.com>
2019-03-03 17:10:49 +0530

    sanders: move wcnss_service to vendor
    Change-Id: I67522d0dc614b739fc571b82ef42f18a936df3be
    Signed-off-by: Arvind Mukund <armu30@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -227,3 +227,5 @@ WIFI_DRIVER_FW_PATH_AP           := "ap"
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_P2P          := "p2p"
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
+PRODUCT_VENDOR_MOVE_ENABLED      := true
+
