e9ff97ee62a04dbfc12e39aa0fb6e4ae0883d631
Arvind Mukund <armu30@gmail.com>
2019-03-08 11:17:45 +0530

    fp_c1212: print bootloader version to recovery UI
    Change-Id: I1f0c750919303e3c3ae1a8f471ce4eec1d79f240
    Signed-off-by: Arvind Mukund <armu30@gmail.com>

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -22,7 +22,7 @@ def FullOTA_InstallEnd(info):
 
 def ReplaceDeviceConfig(info):
   info.script.Mount("/system")
-  info.script.AppendExtra('ui_print("Checking Fingerprint firmware level");')
+  info.script.AppendExtra('ui_print("Installing fingerprint firmware for: " + getprop("ro.bootloader"));')
   info.script.AppendExtra('run_program("/sbin/sh", "/system/etc/patch_fingerprint.sh");')
   info.script.AppendExtra('ifelse(getprop("ro.bootloader") == "0xC212", set_metadata("/system/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-fpcservice", "uid", 0, "gid", 2000, "dmode", 0755, "fmode", 0755, "selabel", "u:object_r:hal_fingerprint_default_exec:s0"));')
   info.script.AppendExtra('ui_print("Checking NFC, compass and DTV support");')
