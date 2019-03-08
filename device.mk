006d9c1bb245ad230eb0893c36ea058b030f6e01
Arvind Mukund <armu30@gmail.com>
2019-03-08 09:37:46 +0530

    patch_fingerprint: move checking and patching to our script
    Change-Id: I095c0d70f82c1bfc3390d853b4161a85a0dc8d54
    Signed-off-by: Arvind Mukund <armu30@gmail.com>

--- device.mk
+++ device.mk
@@ -194,8 +194,9 @@ PRODUCT_PACKAGES += \
     fingerprint.msm8953
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/releasetools/fp_c212.zip:/system/etc/fp_c212.zip \
-    $(LOCAL_PATH)/releasetools/patch_fingerprint.sh:/system/etc/patch_fingerprint.sh
+    $(LOCAL_PATH)/releasetools/fp_c212.zip:system/etc/fp_c212.zip \
+    $(LOCAL_PATH)/releasetools/patch_fingerprint.sh:system/etc/patch_fingerprint.sh
+
 # FM
 PRODUCT_PACKAGES += \
     FMRadio \
--- releasetools/patch_fingerprint.sh
+++ releasetools/patch_fingerprint.sh
@@ -1,14 +1,23 @@
 #!/sbin/sh
+
+firmware_lvl=`getprop ro.bootloader`
+
+echo "Checking bootloader features..."
+echo "This device is running $firmware_lvl bootloader"
+
 #Extract our fp files here
-unzip /system/etc/fp_c212.zip -d /
-#Remove N based FP files
-rm -f "/system/lib/hw/fingerprint.msm8953.so"
-rm -f "/system/lib/lib_fpc_tac_shared.so"
-rm -f "/system/lib/libcom_fingerprints_service.so"
-rm -f "/system/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service.sanders"
-rm -f "/system/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.sanders.rc"
-rm -f "/system/vendor/lib/hw/fingerprint.vendor.msm8953.so"
-echo "persist.qfp=false" >> /system/build.prop
+if [ "$firmware_lvl" = "0xC212" ]; then
+    unzip /system/etc/fp_c212.zip -d /
+    #Remove N based FP files
+    rm -f "/system/lib/hw/fingerprint.msm8953.so"
+    rm -f "/system/lib/lib_fpc_tac_shared.so"
+    rm -f "/system/lib/libcom_fingerprints_service.so"
+    rm -f "/system/vendor/bin/hw/android.hardware.biometrics.fingerprint@2.1-service.sanders"
+    rm -f "/system/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.sanders.rc"
+    rm -f "/system/vendor/lib/hw/fingerprint.vendor.msm8953.so"
+    echo "persist.qfp=false" >> /system/build.prop
+fi
+
 #Cleanup
 rm /system/etc/fp_c212.zip
 rm -- "$0"
