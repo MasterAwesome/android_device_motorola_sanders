68cd3d861527bafa9b4c06a84ffbc034110147ea
ronaxdevil <pratabidya.007@gmail.com>
2019-05-08 15:43:52 +0000

    sanders : update dalvik virtual memory
    Signed-off-by: utsavbalar1231 <utsavbalar1231@gmail.com>
    
    Signed-off-by: sai4041412 <sai404142@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -104,12 +104,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapgrowthlimit=256m \
     dalvik.vm.heapmaxfree=8m \
     dalvik.vm.heapminfree=512k \
-    dalvik.vm.heapsize=384m \
+    dalvik.vm.heapsize=512m \
     dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heaptargetutilization=0.75
+    dalvik.vm.heaptargetutilization=0.75 \
+    ro.dalvik.vm.native.bridge=0
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
