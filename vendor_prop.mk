b32626d6f2a595739fc22bd8e372e983507525a5
kubersharma001 <kubersharma001@gmail.com>
2019-05-20 13:24:55 +0530

    [DNM]sanders: force hal1 on only whatsapp and instagram
    Signed-off-by: sai4041412 <sai404142@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -85,7 +85,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.vidc.dec.downscalar_height=1088 \
     vidc.dec.disable.split.cpu=1 \
     video.disable.ubwc=1 \
-    vendor.camera.aux.packagelist=com.motorola.camera2,com.google.android.Pixel2Mod
+    vendor.camera.aux.packagelist=com.motorola.camera2,com.google.android.Pixel2Mod \
+    camera.hal1.packagelist=com.instagram.android,com.whatsapp
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
