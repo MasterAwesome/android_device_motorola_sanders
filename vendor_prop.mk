e02bff7a525cebd439fcda4ebb3d53af2cf4e9c1
sai4041412 <sai404142@gmail.com>
2019-05-26 08:26:09 +0000

    Sanders: Enable QCOM Boost framework
    Thanks to @kondors1995 for idea
    
    Signed-off-by: sai4041412 <sai404142@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -242,6 +242,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.qc.hardware=true \
     persist.timed.enable=true
 
+#QTI Performance
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.enable_prefetch=1 \
+    vendor.iop.enable_uxe=1 \
+    vendor.iop.enable_prefetch_ofr=1 \
+    vendor.perf.iop_v3.enable=1 \
+    ro.vendor.at_library=libqti-at.so \
+    persist.vendor.qti.games.gt.prof=1
+
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.qmi.adb_logmask=0 \
