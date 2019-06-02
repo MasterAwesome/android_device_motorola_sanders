6e77c14185f7b69fa048bcc3578dd004ad55c8ce
Jason <jaysonedson@gmail.com>
2019-06-02 08:46:49 +0000

    Sanders: Enable touch optimizations
    * Requires "touch response optimizations" patch by zfu <zfu@codeaurora.org>
      in fw/base and fw/native
    
    Signed-off-by: sai4041412 <sai404142@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -339,6 +339,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
 
+# Touch response
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.qti.inputopts.enable=true \
+    persist.vendor.qti.inputopts.movetouchslop=0.6
+
 # USB
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.usb.mtp=0x2e82 \
