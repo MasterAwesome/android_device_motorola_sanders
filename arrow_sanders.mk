ad522aef3a7acd8215307d6d6971eff80863b223
Dibyajyoti Samal <dibyajyoti.samal@gmail.com>
2019-07-20 10:28:14 +0000

    Sanders: Add proper device name
    Signed-off-by: sai4041412 <sai404142@gmail.com>

--- arrow_sanders.mk
+++ arrow_sanders.mk
@@ -38,7 +38,7 @@ PRODUCT_MANUFACTURER := motorola
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="Moto G5S Plus" \
+    PRODUCT_NAME="Moto G (5S) Plus" \
     PRIVATE_BUILD_DESC="sanders-user 8.1.0 OPS28.65-36 9fea release-keys"
 
 # FINGERPRINT
