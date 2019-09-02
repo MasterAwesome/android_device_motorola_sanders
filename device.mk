acf4f39f15608608961b000e7c4092a92631ee2b
sai4041412 <sai404142@gmail.com>
2019-09-02 19:00:00 +0100

    Drop WFD Boot Jars[TEMP]
    Signed-off-by: sai4041412 <sai404142@gmail.com>

--- device.mk
+++ device.mk
@@ -473,7 +473,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
 # Wi-Fi Display
-PRODUCT_BOOT_JARS += \
+#PRODUCT_BOOT_JARS += \
     WfdCommon
 
 # CarrierConfig
