e92807d82f03f9b1364283a70af8a26cec70e591
sai4041412 <sai404142@gmail.com>
2019-09-06 18:50:00 +0100

    build libdexfile_support from logs :     dlopen failed: library "libdexfile_support.so" not found'
    Signed-off-by: sai4041412 <sai404142@gmail.com>

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -6,8 +6,8 @@ LOCAL_PATH := $(call my-dir)
 # However, some of those libs need FWK-ONLY libs, which must be listed here
 # manually.
 VNDK_SP_LIBRARIES := \
-    libdexfile
-
+    libdexfile \
+    libdexfile_support 
 install_in_hw_dir := \
    android.hidl.memory@1.0-impl
 
