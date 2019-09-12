ced88c0fd3b376dad42b7ca5ee74279cc50b2c72
sai4041412 <sai404142@gmail.com>
2019-09-12 17:22:51 +0000

    Revert "sanders: Copy vendor HIDL interface libs to /vendor"
    This reverts commit 92d7dd1157513bf6ca369d5805d4a81c552a6e14.
    
    Signed-off-by: sai4041412 <sai404142@gmail.com>

--- vndk/vndk-sp-libs.mk
+++ vndk/vndk-sp-libs.mk
@@ -31,9 +31,3 @@ VNDK_SP_LIBRARIES := \
     libutils \
     libutilscallstack \
     libdexfile \
-
-EXTRA_VENDOR_LIBRARIES := \
-    vendor.display.config@1.0 \
-    vendor.display.config@1.1 \
-    vendor.nxp.nxpnfc@1.0 \
-    vendor.qti.hardware.camera.device@1.0
