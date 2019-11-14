958989ac2703db503c47e610c99669ea802ca2cd
kubersharma001 <kubersharma001@gmail.com>
2019-11-14 01:42:00 +0100

    Sanders : Switch to Snap
    Change-Id: I139b27ea00655b281037c0769db124b75b3b756a

--- device.mk
+++ device.mk
@@ -108,8 +108,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
-    Snap2 \
-    Camera2 \
+    Snap \
     libgui_vendor \
     camera_msm8953_shim \
     camera.device@1.0-impl \
