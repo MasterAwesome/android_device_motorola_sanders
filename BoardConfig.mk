708c21e1b473ca9f0090ac7bb5ee538d232c1b50
Arvind Mukund <armu30@gmail.com>
2019-11-28 04:32:20 +0530

    remove preopt directives
    Change-Id: I82e0ca825b2964f4139e57b937edcd2c221185cf
    Signed-off-by: Arvind Mukund <armu30@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -128,11 +128,6 @@ BOARD_NO_CHARGER_LED := true
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 
-# Enable dexpreopt to speed boot time
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-WITH_DEXPREOPT := true
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
-
 # Display
 BOARD_USES_ADRENO := true
 OVERRIDE_RS_DRIVER := libRSDriver_adreno.so
