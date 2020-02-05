364601183eb2195b837e3761729f64fc63904302
Lokesh Gidra <lokeshgidra@google.com>
2020-02-05 11:59:12 +0530

    sanders: Modify dalvik-heap device-configs for 4GB devices
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -109,12 +109,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=256m \
-    dalvik.vm.heapmaxfree=8m \
-    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapgrowthlimit=192m \
+    dalvik.vm.heapmaxfree=16m \
+    dalvik.vm.heapminfree=8k \
     dalvik.vm.heapsize=512m \
     dalvik.vm.heapstartsize=8m \
-    dalvik.vm.heaptargetutilization=0.75 \
+    dalvik.vm.heaptargetutilization=0.6 \
     ro.dalvik.vm.native.bridge=0
 
 # Display
