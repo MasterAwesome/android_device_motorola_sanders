5603142e7228bf434db5377c56a20630bbe23f24
dianlujitao <dianlujitao@lineageos.org>
2020-02-18 20:41:29 +0100

    sanders: Uprev NFC HAL to 1.2
    Signed-off-by: Melvin <kartik3163@gmail.com>
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- device.mk
+++ device.mk
@@ -299,7 +299,7 @@ PRODUCT_PACKAGES += \
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.1-service \
+    android.hardware.nfc@1.2-service \
     com.android.nfc_extras \
     libnfc \
     NfcNci \
