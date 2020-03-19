bd3a11118bf3074b0058634a5a3699cbc7ca8898
kubersharma001 <kubersharma001@gmail.com>
2020-03-19 17:45:38 +0530

    Sanders : Set Boot Anim res.
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- arrow_sanders.mk
+++ arrow_sanders.mk
@@ -28,6 +28,7 @@ $(call inherit-product, vendor/arrow/config/common.mk)
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 1920
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := sanders
