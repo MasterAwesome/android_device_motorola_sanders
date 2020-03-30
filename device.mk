543b2539981a5ea22c1c1e0ac19ec5d57005e832
Nick <nikhilnick00007@gmail.com>
2020-03-30 16:44:00 +0000

    Sanders : copy libprotobuf-cpp-full/lite prebuilts from vndk29
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- device.mk
+++ device.mk
@@ -462,6 +462,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vndk-sp
 
+#Vndk prebuilts
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
+    prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
+
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
