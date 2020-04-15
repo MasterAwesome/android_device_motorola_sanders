a687c8c0e68cbd3471ec3f52e5c9864abcbdf9c4
lucasponez <lucasponez@outlook.com>
2020-04-15 11:46:28 +0000

    sanders: Fix ril after blobs update
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- device.mk
+++ device.mk
@@ -395,6 +395,7 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.1 \
     android.hardware.radio@1.2 \
     android.hardware.radio@1.4 \
     android.hardware.radio.config@1.0 \
