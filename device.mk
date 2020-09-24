da27976c4506b4b443767a5d38338baaf560645f
Abhinavgupta371 <abhinavgupta371@gmail.com>
2020-09-24 10:33:00 +0100

    sanders : Switch DRM clearkey to 1.3
    Signed-off-by: ashwatthama <sai404142@gmail.com>
    Signed-off-by: sai4041412 <sai404142@gmail.com>
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- device.mk
+++ device.mk
@@ -161,7 +161,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-service \
     android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # limit dex2oat threads to improve thermals
 PRODUCT_PROPERTY_OVERRIDES += \
