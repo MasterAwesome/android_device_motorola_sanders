3a06d2d11858caac9ffe81c3ecef73059f6ce743
Mikhail Naganov <mikhailnaganov@google.com>
2020-09-29 20:44:00 +0100

    sanders : Switch to Audio HAL V6
    Switch coral and flame to Audio HAL V6
    
    Bug: 141989952
    Test: atest VtsHalAudioV6_0TargetTest
          atest VtsHalAudioEffectV6_0TargetTest
                      manual audio "smoke" test on coral
    
    Change-Id: If40ae28c224160728634736ee95590a779f905c4
    
    Signed-off-by: ashwatthama <sai404142@gmail.com>
    Signed-off-by: sai4041412 <sai404142@gmail.com>
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- device.mk
+++ device.mk
@@ -68,8 +68,8 @@ PRODUCT_AAPT_CONFIG := normal
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@4.0-impl \
-    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio@6.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio@2.0-service \
     android.hardware.broadcastradio@1.0-impl \
     audio.primary.msm8953 \
