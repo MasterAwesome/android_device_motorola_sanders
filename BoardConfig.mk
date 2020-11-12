8b30c373f978ca5b854504eee50692eafb5cb7e4
kerneltoast <sultanxda@gmail.com>
2020-11-12 17:01:43 +0100

    sanders: enable cpuidle during boot
    This is only used to keep cpuidle disabled during boot, which is
    unnecessary and doesn't really improve boot times (but does incur a high
    power cost).
    
    Change-Id: Ieb706115c4ad48f48bc47f650f387be6e66cade5
    Signed-off-by: Genkzsz11 <Genkzsz11@gmail.com>
    Signed-off-by: starlight5234 <starlight5234@protonmail.ch>
    Signed-off-by: sai4041412 <sai404142@gmail.com>
    Signed-off-by: ashwatthama <sai404142@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,7 +73,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 
 # Kernel
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci firmware_class.path=/vendor/firmware_mnt/image loop.max_part=16
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom user_debug=30 msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci firmware_class.path=/vendor/firmware_mnt/image loop.max_part=16
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x80000000
 BOARD_KERNEL_IMAGE_NAME := Image.gz
